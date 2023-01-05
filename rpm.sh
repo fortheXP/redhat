62792de15b7fa1ba251f7bc93b1da96109e49214
fortheXP <89803424+fortheXP@users.noreply.github.com>
2023-01-05 18:02:06 +0530

    Update rpm.sh
    Update to handle 3 letter month name like Jan as well

--- rpm.sh
+++ rpm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the current time in Unix timestamp format
+#current_time=$(date +%s)
+
 # Get the time 24 hours ago in Unix timestamp format
 time_24h_ago=$(date --date='24 hours ago' +%s)
 
@@ -9,21 +12,23 @@ rpm -qa --last | awk '
   function getMonthNumber(month) {
     split("January February March April May June July August September October November December", months)
     for (i in months) {
-      if (month == months[i]) {
-        return i
+      #if (month == months[i]) {
+       if (tolower(substr(month, 1, 3)) == tolower(substr(months[i], 1, 3))) {
+          return  i  
       }
     }
-    return 1
+    return "01"
   }
 
   # Split the line into fields
   { split($6, t, ":"); r[1] = $5; r[2] = $4; r[3] = $3; r[4] = t[1]; r[5] = t[2]; r[6] = t[3]; r[7] = $7 }
 
   # Convert the date and time fields into a Unix timestamp
-  { etime = (r[1]" "r[3]" "getMonthNumber(r[2])" "r[4]" "r[5]" "r[6]" "r[7])
+  { etime = (r[1]" "getMonthNumber(r[2])" "r[3]" "r[4]" "r[5]" "r[6]" "r[7])
     epoch = mktime(etime) }
 
   # Check if the package was upgraded within the past 24 hours
-  epoch > "'"$time_24h_ago"'" {
-    print $0" "etime
+  epoch > "'"$time_24h_ago"'"  {
+    print $1
   }'
+
